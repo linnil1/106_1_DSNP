18347934e5b50292ff9f680e0ae23fd4783d6db8
linnil1 <linnil1.886@gmail.com>
2018-01-17 01:43:44 +0800

    add fraig

--- fraig/src/cir/cirFraig.cpp
+++ fraig/src/cir/cirFraig.cpp
@@ -60,6 +60,101 @@ void CirMgr::goStrash(ID id)
 
 void CirMgr::fraig()
 {
+  if (!_groupMax)
+    return;
+  CirGate::setVisitFlag();
+  mergeStr = "Fraig";
+  _hashMap.init(GateAnd::getNum() << 2); // 4
+  _solver.initialize();
+  _fecNow = vector<IdList>(_groupMax + 1);
+  _fecNow[1].push_back(0);
+  for (ID &id: _ins)
+    getGate(id)->setSatVar(_solver.newVar());
+  getGate(0)->setSatVar(_solver.newVar());
+  for (ID &id: _outs)
+    goFraig(id);
+  _hashMap.reset();
+
+  // full fraig
+  _groupMax = 0;
+  _fecCollect.clear();
+  for (ID &id:_FECs)
+    if (getGate(id >> 1))
+      getGate(id >> 1)->setFec(0);
+  _FECs.resize(1);
+  _simStart = 1;
+}
+
+// fraig strash opt all in one
+// didn't make sure that no unused gate in input
+void CirMgr::goFraig(ID id)
+{
+  CirGate *gate = getGate(id);
+  assert(gate);
+  if (gate->isVisit()) return ;
+  for (unsigned i=0; i<gate->fanInSize(); ++i)
+    goFraig(gate->getFanin()[i] >> 1);
+  if (!gate->isAig())
+    return ;
+
+  // opt
+  for (unsigned i=0; i<2; ++i)
+    if (gate->getFanin()[i] == 0) {
+      merge(id, 0);
+      return ;
+    }
+    else if (gate->getFanin()[i] == 1) {
+      merge(id, gate->getFanin()[i ^ 1]);
+      return ;
+    }
+  if ((gate->getFanin()[0] ^ gate->getFanin()[1]) == 0) {
+    merge(id, gate->getFanin()[0]);
+    return ;
+  }
+  else if ((gate->getFanin()[0] ^ gate->getFanin()[1]) == 1) {
+    merge(id, 0);
+    return ;
+  }
+
+  // strash
+  GateAnd *gateAnd = static_cast<GateAnd*>(gate);
+  ID *to = _hashMap.insert(gateAnd, id << 1);
+  if (to) {
+    merge(id, *to);
+    return ;
+  }
+
+  // fraig
+  unsigned g = gate->getFec();
+  IdList &v = _fecNow[g];
+  for (ID &i: v) {
+    cout << "Proof : " << i << ' ' << id << endl;
+    // solve by SAT
+    CirGate *newGate = getGate(i);
+    Var newV = _solver.newVar();
+    bool inv = newGate->getSim() != gate->getSim();
+    _solver.addXorCNF(newV, gate->getSatVar()   , inv  ,
+                            newGate->getSatVar(), false);
+    _solver.assumeRelease();
+    if (i == 0)
+      _solver.assumeProperty(getGate(0)->getSatVar(), false);
+    _solver.assumeProperty(newV, true);
+
+    // functionally same -> merge it
+    if (!_solver.assumpSolve()) {
+      cout << "SAT ";
+      merge(id, (i << 1) | inv);
+      *_hashMap.insert(gateAnd, 0) = (i << 1) | inv;
+      return ;
+    }
+  }
+  // no return
+  if (g)
+    v.push_back(id);
+  _solver.addAigCNF(gate->getSatVar(), getGate(gate->getFanin()[0] >> 1)->getSatVar(),
+                                       gate->getFanin()[0] & 1,
+                                       getGate(gate->getFanin()[1] >> 1)->getSatVar(),
+                                       gate->getFanin()[1] & 1);
 }
 
 /********************************************/
